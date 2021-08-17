32cf57af5be45fb1efd81d4afbde8a97ffe4add2
Darko <darko.stupar.03@gmail.com>
2021-08-17 15:54:54 +0200

    Napravio sam sistem za pogadjanje brojeva.

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+
+print('pogodi broj,qwertyuiop = 12345678910')
+
+x=randint(1,10)
+
+
+a=int(input('unesi broj:'))
+
+if x==a:
+    print('uspesno')
+else:
+    print('Pokusaj ponovo')
